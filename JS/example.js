3523365afa88ad6902373693751b0f981c99d051
Kukyeon <rnrdus2000@naver.com>
2025-09-18 15:48:15 +0900

    https://github.com/Kukyeon/javaScript_workspace.git

---
+++ JS/example.js
@@ -0,0 +1,8 @@
+document.write("hello World","헬러오우 월드으으");
+
+var str = "kor";
+var str2 = "jap";
+var num1 = 100;
+var num2 = Number("100"); // 문자 100-> 숫자100
+var tag1 = "<h1> String </h1>";
+var bool1 = Boolean(0); // false -> 0,null 이 아닌 값은 모두 true
